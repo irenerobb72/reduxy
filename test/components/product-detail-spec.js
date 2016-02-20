0df6fd79bacc09fc26872ce7521d6319d0eaef18
irene robb <email": null>
2016-02-21 11:32:03 +1300

    adding 2 new untested tests

--- test/components/product-detail-spec.js
+++ test/components/product-detail-spec.js
@@ -10,5 +10,12 @@ import { mount, render, shallow } from 'enzyme'
 import {ProductDetail} from '../app/components/product-detail.jsx'
 
 describe('<ProductDetail/>', () => {
-  it()
+  it('renders without any props', () => {
+    const wrapper = shallow(<ProductDetail/>)
+    expect(wrapper).to.be.ok
+  })
+  it('has a classname of product-detail', () => {
+    const wrapper = shallow(<ProductDetail/>)
+    expect(wrapper).to.have.className('product-detail')
+  })
 })
